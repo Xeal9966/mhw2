557880517560a07042d603b5acc31571140cbb24
SollamiGabriele <81163490+Xeal9966@users.noreply.github.com>
2021-04-10 21:17:10 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,97 @@
+const lista = [{
+    img:"image/cyberpunk-2077-cover.jpg",
+    nome : "Cyberpunk 2077",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/halo.jpg",
+    nome : "Halo Infite",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/fifa21.jpg",
+    nome : "FIFA 21",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/tetra-preorder-standard-edition-epic-store-portrait-1200x1600-1200x1600-430971222.png",
+    nome : "FarCry 6",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
+    nome : "The Legend of Zelda",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/God_of_War_4_cover.jpg",
+    nome : "God of War 4",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/luigi-cover-art-1440x2560.jpg",
+    nome : "Luigi's Mansion 2",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/AUG201626.jpg",
+    nome : "Horizon Zero Down",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/the last of us pt2.jpg",
+    nome : "The Last of Us 2",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/heavy-rain-cover.jpg",
+    nome : "Heavy Rain",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/destiny-2-cover.jpg",
+    nome : "Destiny 2",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+},
+{
+    img:"image/It's_about_time.png",
+    nome : "Crash Bandicoot 4",
+    prezzo: "€ 49,99" ,
+    icon: "image/icons/shopping_cart-black-18dp 1.svg",
+    show: "Mostra tutto",
+    des: "Questa descrizione è un placeholder utilizzato ai fini dell'homework2, esso non ha nessuna associazione con l'elemento a cui è associato ma è utilizzato unicamente a fini dimostrativi"
+}
+]
